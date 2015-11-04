304f6fcc28bbc352d10d71847812288733cf916b
David Simner <david.simner@red-gate.com>
2015-11-04 15:36:12 +0000

    to help the next diff

--- Crypto/X509CertificateExtensions.cs
+++ Crypto/X509CertificateExtensions.cs
@@ -1,9 +1,13 @@
-﻿using Org.BouncyCastle.X509;
+using Org.BouncyCastle.X509;
 
 namespace Crypto
 {
     public static class X509CertificateExtensions
     {
+
+
+
+
         public static void ThrowIfChainNotOkay(this X509Certificate leaf, X509Certificate root)
         {
             var rootPublicKey = root.GetPublicKey();
